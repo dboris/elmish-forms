d38345a402f809ebf044cd520423ca7058d1034f
Boris D. <boris.dob@gmail.com>
2017-09-08 15:54:34 -0700

    Add PCL project.

--- Elmish.Forms/Extensions.fs
+++ Elmish.Forms/Extensions.fs
@@ -1,20 +1,23 @@
-﻿module Elmish.Program
+﻿[<RequireQualifiedAccess>]
+module Elmish.Forms.Program
 
-    let runPage (page : Xamarin.Forms.Page) (program : Program<unit, 'model, 'msg, Elmish.Forms.ViewBindings<'model, 'msg>>) = 
+open Elmish
 
-        let mutable lastModel = None
+let runPage (page : Xamarin.Forms.Page) (program : Program<unit, 'model, 'msg, ViewBindings<'model, 'msg>>) = 
 
-        let setState model dispatch = 
-            match lastModel with
-            | None -> 
-                let mapping = program.view model dispatch
-                let vm = Elmish.Forms.ViewModelBase<'model, 'msg> (model, dispatch, mapping)
-                page.BindingContext <- vm
-                //console.log <| sprintf "VM mapping: %A" mapping
-                lastModel <- Some vm
-            | Some vm ->
-                vm.UpdateModel model
-                      
-        // Start Elmish dispatch loop  
-        { program with setState = setState } 
-        |> Elmish.Program.run
+    let mutable lastModel = None
+
+    let setState model dispatch = 
+        match lastModel with
+        | None -> 
+            let mapping = program.view model dispatch
+            let vm = ViewModelBase<'model, 'msg> (model, dispatch, mapping)
+            page.BindingContext <- vm
+            //console.log <| sprintf "VM mapping: %A" mapping
+            lastModel <- Some vm
+        | Some vm ->
+            vm.UpdateModel model
+                  
+    // Start Elmish dispatch loop  
+    { program with setState = setState } 
+    |> Program.run
